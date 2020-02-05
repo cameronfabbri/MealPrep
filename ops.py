02f3c04d376fd5d53fabb2024ee8920c88f3e2fd
cameronfabbri <cameronfabbri@gmail.com>
2020-02-04 23:07:02 -0600

    switched to bing

--- ops.py
+++ ops.py
@@ -4,28 +4,8 @@
 # Copyright (c) 2019.
 # Cameron Fabbri
 
-from google_images_download import google_images_download
-
-
+from BingImages import BingImages
 
 def get_image_url(keywords):
-
-    print('\n\nDOWNLOADING URL\n\n')
-
     keywords = ','.join(keywords)
-
-    response  = google_images_download.googleimagesdownload()
-    arguments = {'keywords':keywords,'limit':1,'print_urls':True,'no_download':True, 'silent_mode':True}
-    image_url_ = response.download(arguments)[0][keywords]
-
-    img_count = 0
-    while image_url_ == []:
-        img_count += 1
-        image_url_ = response.download(arguments)[0][keywords]
-        #if img_count > 20: 
-        #    image_url_ = [None]
-        #else:
-        #    image_url_ = response.download(arguments)[0][title]
-
-    image_url = image_url_[0]
-    return image_url
+    return list(BingImages(keywords, count=1).get())[0]
