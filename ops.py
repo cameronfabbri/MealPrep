9a79d9e07ebcddb16d3f8efea08c4cb3606b37fe
cameronfabbri <cameronfabbri@gmail.com>
2020-02-11 22:22:37 -0600

    added temp

--- ops.py
+++ ops.py
@@ -8,4 +8,9 @@ from BingImages import BingImages
 
 def get_image_url(keywords):
     keywords = ','.join(keywords)
-    return list(BingImages(keywords, count=1).get())[0]
+    print('keywords:',keywords)
+    try:
+        a = list(BingImages(keywords, count=1).get())[0]
+    except:
+        a = 'https://lh3.googleusercontent.com/proxy/V0ZSsia8R9xFWJyKQjwsYO3Mp55UdlFyeFtS7RR4xq6gLTD_HZnGTKOG5_Efc_EFTEfGRQdH4CmnUwU7LfLfIwQQWFcF7ErVsM0vnS3N4XWNlyOQxfkXrliG1w'
+    return a
